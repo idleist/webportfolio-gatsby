8aaafb457bc089693284de61652d429f4b222817
benrugman <benrugman@yahoo.co.uk>
2019-06-16 21:26:05 +0100

    completed form floating label functionality

---
+++ src/components/contact.js
@@ -0,0 +1,99 @@
+import React from "react"
+import styled from "styled-components"
+import "./contact.scss"
+
+import contactBgsm from "../assets/svg/contactbgsm.svg"
+import contactBgmd from "../assets/svg/contactbgmd.svg"
+import contactBg from "../assets/svg/contactbg.svg"
+
+const ContactSection = styled.section`
+  width: 100%;
+  background: url(${contactBgsm}) no-repeat;
+  background-size: 100%;
+
+  @media screen and (min-width: 600px) {
+    background: url(${contactBgmd}) no-repeat;
+    background-size: 100%;
+  }
+
+  @media screen and (min-width: 900px) {
+    background: url(${contactBg}) no-repeat;
+    background-size: 100%;
+  }
+`
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+      float: true,
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() {
+    const formInputs = document.querySelectorAll(".form-input")
+
+    formInputs.forEach(formInput => {
+      formInput.addEventListener("focus", e => {
+        e.target.parentElement.classList.add("active")
+      })
+
+      formInput.addEventListener("blur", e => {
+        if (!e.target.value) {
+          e.target.parentElement.classList.remove("active")
+        }
+      })
+    })
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: [e.target.value] })
+  }
+
+  render() {
+    return (
+      <ContactSection id="contact">
+        <div className="form-container">
+          <h2 className="form-title">contact</h2>
+          <form className="form">
+            <div className="form-input-container">
+              <label className="form-label">Name</label>
+              <input
+                className="form-input"
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-input-container">
+              <label className="form-label">Email</label>
+              <input
+                className="form-input"
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-input-container">
+              <label className="form-label">Message</label>
+              <textarea
+                className="form-input"
+                name="message"
+                value={this.state.message}
+                onChange={this.handleChange}
+              />
+            </div>
+            <input type="submit" value="SEND MESSAGE" />
+          </form>
+        </div>
+      </ContactSection>
+    )
+  }
+}
+
+export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import Skills from "../components/skills"
 import ProjectContainer from "../components/projects"
 import About from "../components/about"
+import Contact from "../components/contact"
 import "../styles/base.scss"
 
 export default () => (
@@ -11,6 +12,7 @@ export default () => (
       <Skills />
       <ProjectContainer />
       <About />
+      <Contact />
     </div>
   </Layout>
 )
